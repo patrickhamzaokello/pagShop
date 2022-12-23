372c653101545d323d308bc4524de1203e2e9195
Pkasemer <41443166+patrickhamzaokello@users.noreply.github.com>
2022-12-23 08:30:47 +0300

    show category and display overlay

--- shop/categories.js
+++ shop/categories.js
@@ -3,19 +3,36 @@ readyCategories();
 
 
 function viewCategory(){
+    // Get the product ID
+    var productId = 123;
+    // Make the AJAX call
+    var xhr = new XMLHttpRequest();
+    xhr.onreadystatechange = function() {
+        if (xhr.readyState === 4 && xhr.status === 200) {
+            // Parse the JSON data
+            var data = JSON.parse(xhr.responseText);
+
+            // Clear the overlay content
+            overlayContent.innerHTML = '';
+
+            // Iterate through the products and add them to the overlay content
+            data.forEach(function(product) {
+                var productElement = document.createElement('div');
+                productElement.innerHTML = '<h3>' + product.name + '</h3>';
+                productElement.innerHTML += '<p>' + product.standard_charge + '</p>';
+                overlayContent.appendChild(productElement);
+            });
+        }
+    };
+    xhr.open('GET', 'category_charge.php?id=' + productId, true);
+    xhr.send();
+
     const overlay = document.getElementById('overlay');
     overlay.style.display = 'block';
 }
 
 function readyCategories() {
-// Get the button and the overlay element
-    var cat_view_btn = document.getElementsByClassName('cat-view');
 
-    console.log(cat_view_btn);
-    for (var i = 0; i < cat_view_btn.length; i++) {
-        var button = cat_view_btn[i];
-        button.addEventListener("click", viewCategory);
-    }
 // Get a reference to the search input and results container
     var searchInput = document.getElementById('searchTerm');
     var resultsContainer = document.getElementById('results');
@@ -44,6 +61,14 @@ function readyCategories() {
             for (var i = 0; i < filteredProducts.length; i++) {
                 var product = filteredProducts[i];
                 resultsContainer.innerHTML += `<div class="cat-product-box"> <h2 class="cat-product-title">${product.name}</h2> <span class="cat-description">${product.description}</span> <i class="bx bx-shopping-bag cat-view"></i></div>`;
+            }
+            // Get the button and the overlay element
+            var cat_view_btn = document.getElementsByClassName('cat-view');
+
+            console.log(cat_view_btn);
+            for (var i = 0; i < cat_view_btn.length; i++) {
+                var button = cat_view_btn[i];
+                button.addEventListener("click", viewCategory);
             }
         }
     });
---
+++ shop/category_charge.php
@@ -0,0 +1,45 @@
+<?php
+
+// Connect to the MySQL database
+$host = 'localhost';
+$user = 'root';
+$password = '';
+$dbname = 'rgxszumy_hospital_pagmh';
+$conn = mysqli_connect($host, $user, $password, $dbname);
+
+// Check if the connection was successful
+if (!$conn) {
+    http_response_code(500);
+    exit;
+}
+
+// Check if the ID was provided in the query string
+$id = isset($_GET['id']) ? (int)$_GET['id'] : 0;
+
+// Build the SQL query
+$sql = 'SELECT * FROM charges';
+if ($id) {
+    $sql .= ' WHERE charge_category_id = ' . $id;
+}
+
+// Execute the query
+$result = mysqli_query($conn, $sql);
+
+// Check if the query was successful
+if (!$result) {
+    http_response_code(500);
+    exit;
+}
+
+// Fetch the rows as an array
+$products = mysqli_fetch_all($result, MYSQLI_ASSOC);
+
+// Close the connection
+mysqli_close($conn);
+
+// Set the content type to JSON
+header('Content-Type: application/json');
+
+// Convert the array to JSON and echo it
+echo json_encode($products);
+
