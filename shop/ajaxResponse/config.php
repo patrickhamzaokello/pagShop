fd2097bbf752e139c7e3c65cfe6c85c057806b4c
Pkasemer <41443166+patrickhamzaokello@users.noreply.github.com>
2022-12-26 09:53:30 +0300

    update on config2

--- shop/ajaxResponse/config.php
+++ shop/ajaxResponse/config.php
@@ -1,9 +1,11 @@
 <?php
 // Connect to the MySQL database
 $host = 'localhost';
-$user = 'rgxszumy_pagshop';
-$password = 'wZ0fx9uWJ[@G';
+$user = 'root';
+$password = '';
 $dbname = 'rgxszumy_hospital_pagmh';
+//$user = 'rgxszumy_pagshop';
+//$password = 'wZ0fx9uWJ[@G';
 
 $conn = new mysqli($host, $user, $password, $dbname);
 
