5a5cd7181d3f3b8538185cd4051c8c6cfe22b3a8
Pkasemer <41443166+patrickhamzaokello@users.noreply.github.com>
2023-04-03 00:20:59 +0300

    fixed

--- shop/report.php
+++ shop/report.php
@@ -2,31 +2,106 @@
 <html>
 <head>
     <meta charset="UTF-8">
-    <title>Sales Report</title>
+    <title>PAG Hospital Sales Report</title>
     <style>
         /* Add CSS styles to format the table */
+         body {
+             margin: 0 auto;
+             text-align: center;
+         }
+
+        .date_user {
+            text-align: center;
+            text-transform: capitalize;
+        }
         table {
+            width: 750px;
             border-collapse: collapse;
-            width: 100%;
+            margin:10px auto;
         }
-        th, td {
-            text-align: left;
-            padding: 8px;
+
+        /* Zebra striping */
+        tr:nth-of-type(odd) {
+            background: #eee;
         }
+
         th {
-            background-color: #4CAF50;
+            background: #3498db;
             color: white;
+            font-weight: bold;
         }
-        tr:nth-child(even) {
-            background-color: #f2f2f2;
+
+        td, th {
+            padding: 10px;
+            border: 1px solid #ccc;
+            text-align: left;
+            font-size: 18px;
+        }
+
+        /*
+        Max width before this PARTICULAR table gets nasty
+        This query will take effect for any screen smaller than 760px
+        and also iPads specifically.
+        */
+        @media
+        only screen and (max-width: 760px),
+        (min-device-width: 768px) and (max-device-width: 1024px)  {
+
+            table {
+                width: 100%;
+            }
+
+            /* Force table to not be like tables anymore */
+            table, thead, tbody, th, td, tr {
+                display: block;
+            }
+
+            /* Hide table headers (but not display: none;, for accessibility) */
+            thead tr {
+                position: absolute;
+                top: -9999px;
+                left: -9999px;
+            }
+
+            tr { border: 1px solid #ccc; }
+
+            td {
+                /* Behave  like a "row" */
+                border: none;
+                border-bottom: 1px solid #eee;
+                position: relative;
+                padding-left: 50%;
+            }
+
+            td:before {
+                /* Now like a table header */
+                position: absolute;
+                /* Top/left values mimic padding */
+                top: 6px;
+                left: 6px;
+                width: 45%;
+                padding-right: 10px;
+                white-space: nowrap;
+                /* Label the data */
+                content: attr(data-column);
+
+                color: #000;
+                font-weight: bold;
+            }
+
         }
     </style>
 </head>
 <body>
-<h1>Sales Report</h1>
-<p>Select a date:</p>
-<input type="date" id="dateInput">
-<button onclick="getSalesReport()">Get Report</button>
+<h1>PAG Hospital Sales Report</h1>
+<div class="date_user">
+    <p>Select a date:</p>
+    <input type="date" id="dateInput">
+    <button onclick="getSalesReport()">Get Report</button>
+    <button onclick="window.print()">Print Report</button>
+
+</div>
+<p id="reportDate"></p>
 <table id="salesTable">
     <thead>
     <tr>
@@ -39,6 +114,8 @@
     <tbody id="salesTableBody">
     </tbody>
 </table>
+<p>Total Sales: <span id="totalSales"></span></p>
+
 <script>
     function getSalesReport() {
         const date = document.getElementById("dateInput").value;
@@ -50,7 +127,11 @@
 
     function displaySalesReport(data) {
         const salesTableBody = document.getElementById("salesTableBody");
+        const totalSalesElem = document.getElementById("totalSales");
+        const reportDateElem = document.getElementById("reportDate");
+        const date = document.getElementById("dateInput").value;
         salesTableBody.innerHTML = "";
+        let totalSales = 0;
         data.forEach(item => {
             const row = salesTableBody.insertRow();
             const itemNameCell = row.insertCell();
@@ -61,7 +142,10 @@
             numItemsSoldCell.textContent = item.num_items_sold;
             const totalSalesCell = row.insertCell();
             totalSalesCell.textContent = item.total_sales;
+            totalSales += parseFloat(item.total_sales);
         });
+        totalSalesElem.textContent = `UGX ${totalSales.toFixed(0)}`;
+        reportDateElem.textContent = `Sales Report for ${date}`
     }
 </script>
 </body>
